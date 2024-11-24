31bfa50a71de1de47338d1ab2e401b015df3d4d5
Freezler <rldevries79@hotmail.com>
2024-11-24 13:35:22 +0100

    init commit: basic crud demo

---
+++ index.js
@@ -0,0 +1,143 @@
+const path = require('path');
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid'); //For generating ID's
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware i.e. functions that run between request and response
+app.use(express.static(path.join(__dirname, 'public')))
+//To parse form data in POST request body:
+app.use(express.urlencoded({ extended: true }))
+// To parse incoming JSON in POST request body:
+app.use(express.json())
+// To 'fake' put/patch/delete requests:
+app.use(methodOverride('_method'))
+// Views folder and EJS setup:
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Our fake database:
+let comments = [
+    {
+        id: uuid(),
+        username: 'Raymond',
+        comment: 'Cool crud app you built there !',
+        lastUpdated: ''
+    },
+    {
+        id: uuid(),
+        username: 'Anonymous',
+        comment: 'lol that is so funny! because its true',
+        lastUpdated: ''
+    },
+    {
+        id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd',
+        lastUpdated: ''
+    },
+    {
+        id: uuid(),
+        username: 'general Pavlov',
+        comment: 'All your base are belong to us',
+        lastUpdated: ''
+    }
+]
+// **********************************
+// INDEX - renders multiple comments
+// **********************************
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+// **********************************
+// NEW - renders a form
+// **********************************
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+// **********************************
+// CREATE - creates a new comment
+// **********************************
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() })
+    res.redirect('/comments');
+})
+// *******************************************
+// SHOW - details about one particular comment
+// *******************************************
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+})
+// *******************************************
+// EDIT - renders a form to edit a comment
+// *******************************************
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+// *******************************************
+// UPDATE - updates a particular comment
+// *******************************************
+app.patch("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find((c) => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect("/comments");
+});
+
+// *******************************************
+// DELETE/DESTROY- removes a single comment
+// *******************************************
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
+
+
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
+})
+
+app.listen(port, () => {
+    console.log(`ON PORT ${port}!`)
+})
+
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
